aac7db94f87a4b32365fec5f62e9aafa6c9cf486
puradchi10 <136870052+puradchi10@users.noreply.github.com>
2023-06-17 20:31:44 +0530

    Add files via upload

---
+++ my first project 11.py
@@ -0,0 +1,11 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Pc/Desktop/ppppp/myfirstprogram.py
+Hello, world ! 
+>>> print('Hello, world ! ')
+Hello, world ! 
+>>> person=input('what is your name?')
+what is your name?puradsi
+>>> print('Hello',person)
+Hello puradsi
